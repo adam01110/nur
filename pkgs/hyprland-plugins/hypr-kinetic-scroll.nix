017b6e8c9216aa4ba3c91d3eb95838effb501be4
Adam0 <alumail@protonmail.com>
2026-07-13 21:00:23 -0400

    pin to stable - kinetic scroll

--- pkgs/hyprland-plugins/hypr-kinetic-scroll.nix
+++ pkgs/hyprland-plugins/hypr-kinetic-scroll.nix
@@ -7,13 +7,13 @@
 }:
 mkHyprlandPlugin {
   pluginName = "hypr-kinetic-scroll";
-  version = "0-unstable-2026-07-05";
+  version = "0.4.0";
 
   src = fetchFromGitHub {
     owner = "savonovv";
     repo = "hypr-kinetic-scroll";
-    rev = "378e29c8ec9650965fe2713d53d1fff3ce137003";
-    hash = "sha256-Vt8l9PMAgcmbBynqLa91N2nW0Caij52YUYg3mWXYwSE=";
+    rev = "1e77fb637b18bcc9d1f76f54212f5881d8b9223c";
+    hash = "sha256-rYhrHXLqMOkiTCgub2s9s4CXoNkTrWq9Qsggq6oGjlQ=";
   };
 
   installPhase = ''
@@ -24,12 +24,12 @@ mkHyprlandPlugin {
     runHook postInstall
   '';
 
-  meta = {
+  meta = with lib; {
     # keep-sorted start
     description = "Hyprland plugin providing compositor-level kinetic scrolling for touchpads";
     homepage = "https://github.com/savonovv/hypr-kinetic-scroll";
-    license = lib.licenses.mit;
-    platforms = lib.platforms.linux;
+    license = licenses.mit;
+    platforms = platforms.linux;
     # keep-sorted end
   };
 }
